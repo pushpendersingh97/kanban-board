86071d53157958eea2e786251fbcd52d99b0bd17
PushpenderSingh98 <Pushjony@98>
2020-09-22 00:29:33 +0530

    Basic form ready for create ticket

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 const materialModules = [
+  MatIconModule,
   MatButtonModule,
-  MatCardModule
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
