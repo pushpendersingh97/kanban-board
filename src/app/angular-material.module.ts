fb50f3051535aac8cce2a69f945fe6bbc4ee6443
PushpenderSingh98 <Pushjony@98>
2020-09-21 23:35:13 +0530

    New Ticket UI updated a little

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 const materialModules = [
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule
 ];
 
 @NgModule({
