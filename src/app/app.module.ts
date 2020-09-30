ce5e7c1598beef0118bb7e05ad375aed47c11f84
PushpenderSingh98 <Pushjony@98>
2020-09-30 21:13:44 +0530

    Including Form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { TicketState } from './services/ticket-state.service';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AngularMaterialModule } from './angular-material.module';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainViewComponent } from './main-view/main-view.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
+import { TicketState } from './services/ticket-state.service';
+import { MainViewComponent } from './main-view/main-view.component';
 import { DialogEntryComponent, HeaderComponent } from './header/header.component';
+
 import { TicketDialogComponent } from './ticket-dialog/ticket-dialog.component';
 
 @NgModule({
@@ -27,7 +29,9 @@ import { TicketDialogComponent } from './ticket-dialog/ticket-dialog.component';
     BrowserAnimationsModule,
     DragDropModule,
     CommonModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [TicketState],
   bootstrap: [AppComponent],
